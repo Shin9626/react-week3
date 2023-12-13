46207c070f208e54f7eaa00b3d2a6b811b1611c3
SHIN <akiyachiumi@gmail.com>
2023-12-13 20:24:14 +0800

    fixed: router path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Pages/Home";
 import Spots from "./Pages/Spots";
@@ -6,7 +6,7 @@ import Error from "./Pages/Error";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/react-week-3/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -14,7 +14,7 @@ function App() {
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
